70dfe5fb66c8d4589afe54fae89aad93793e6447
Fabien <fabien.le-fichoux@epitech.eu>
2024-01-22 15:28:31 +0100

    Add message Routes

---
+++ app/routes/MessageRoutes.js
@@ -0,0 +1,11 @@
+module.exports = router;
+const express = require('express');
+const router = express.Router();
+const messageController = require('../controllers/MessageController');
+
+router.post('/messages', messageController.createMessage);
+router.get('/messages/:messageId', messageController.getMessage);
+router.put('/messages/:messageId', messageController.updateMessage);
+router.delete('/messages/:messageId', messageController.deleteMessage);
+
+module.exports = router;
