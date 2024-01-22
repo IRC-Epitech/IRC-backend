3ed8ae7a565719420509ba47766769845feec23b
Fabien <fabien.le-fichoux@epitech.eu>
2024-01-22 15:06:32 +0100

    Add channel controller

--- app/controllers/ChannelController.js
+++ app/controllers/ChannelController.js
@@ -1,13 +1,51 @@
 const Channel = require('../models/ChannelModel');
 
+// Créer un canal
 exports.createChannel = async (req, res) => {
     try {
         const newChannel = new Channel(req.body);
         await newChannel.save();
         res.status(201).send(newChannel);
     } catch (error) {
-        res.status(400).send(error);
+        res.status(500).send(error);
     }
 };
 
-// Ajoutez ici d'autres méthodes comme getChannel, updateChannel, deleteChannel, etc.
+// Lire un canal
+exports.getChannel = async (req, res) => {
+    try {
+        const channel = await Channel.findById(req.params.channelId);
+        if (!channel) {
+            return res.status(404).send();
+        }
+        res.status(200).send(channel);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+// Mettre à jour un canal
+exports.updateChannel = async (req, res) => {
+    try {
+        const channel = await Channel.findByIdAndUpdate(req.params.channelId, req.body, { new: true });
+        if (!channel) {
+            return res.status(404).send();
+        }
+        res.status(200).send(channel);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+// Supprimer un canal
+exports.deleteChannel = async (req, res) => {
+    try {
+        const channel = await Channel.findByIdAndDelete(req.params.channelId);
+        if (!channel) {
+            return res.status(404).send();
+        }
+        res.status(200).send(channel);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
