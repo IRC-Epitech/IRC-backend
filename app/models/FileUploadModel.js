a53f0beeef4d6f2c92fb4dc2906eb5328f625f5d
Fabien <fabien.le-fichoux@epitech.eu>
2024-01-23 11:34:52 +0100

    chnage file upload with french date

--- app/models/FileUploadModel.js
+++ app/models/FileUploadModel.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const { toFrenchTime } = require('../utils/transformDate');
 
 const FileUploadSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User' },
     filePath: String,
     uploadDate: { type: Date, default: Date.now },
 });
-
+FileUploadSchema.methods.getFrenchUploadDate = function() {
+    return toFrenchTime(this.uploadDate);
+};
 const FileUpload = mongoose.model('FileUpload', FileUploadSchema);
-
 module.exports = FileUpload;
