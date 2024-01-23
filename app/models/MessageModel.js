04dd07777b99881e65615d01d564d7251185e2d7
Fabien <fabien.le-fichoux@epitech.eu>
2024-01-23 11:35:06 +0100

    chnage message with french date

--- app/models/MessageModel.js
+++ app/models/MessageModel.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const { toFrenchTime } = require('../utils/transformDate');
+
 
 const messageSchema = new Schema({
     content: String,
     user: { type: Schema.Types.ObjectId, ref: 'User' },
     timestamp: { type: Date, default: Date.now },
 });
+messageSchema.methods.getFrenchTimestamp = function() {
+    return toFrenchTime(this.timestamp);
+};
 
 const Message = mongoose.model('Message', messageSchema);
 module.exports = Message;
