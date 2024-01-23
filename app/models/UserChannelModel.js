f3cf3016e7b56653cebdfac73d5f234f7ffebe12
Fabien <fabien.le-fichoux@epitech.eu>
2024-01-23 16:08:43 +0100

    delete french date

--- app/models/UserChannelModel.js
+++ app/models/UserChannelModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const { toFrenchTime } = require('../utils/transformDate');
 
 
 const userChannelSchema = new Schema({
@@ -8,9 +7,7 @@ const userChannelSchema = new Schema({
     channel: { type: Schema.Types.ObjectId, ref: 'Channel' },
     joinedDate: { type: Date, default: Date.now },
 });
-userChannelSchema.methods.getFrenchJoinedDate = function() {
-    return toFrenchTime(this.joinedDate);
-};
+
 const UserChannelModel = mongoose.model('UserChannel', userChannelSchema);
 
 module.exports = UserChannelModel;
