31137903b044532f5c892c92f5f4060d23610fde
Fabien <fabien.le-fichoux@epitech.eu>
2024-01-30 14:27:41 +0100

    Change model creator for database

--- app/utils/mongodb_schema_database.js
+++ app/utils/mongodb_schema_database.js
@@ -1,29 +1,45 @@
-const mongoose = require('mongoose');
+
+const MongoClient = require('mongodb').MongoClient;
 const bcrypt = require('bcrypt');
-const User = require('../models/UserModel');
-const Message = require('../models/MessageModel');
-const Channel = require('../models/ChannelModel');
-const FileUpload = require('../models/FileUploadModel');
-const PrivateMessage = require('../models/PrivateMessageModel');
-const UserChannel = require('../models/UserChannelModel');
+const url = 'mongodb://localhost:27017/';
 
-const url = 'mongodb://localhost:27017/IRC';
+MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
+    if (err) throw err;
+    const db = client.db('myDatabase'); // Remplacez 'myDatabase' par le nom de votre base de données
 
-mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true })
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err));
+    // Schéma de la collection Utilisateurs
+    const users = db.collection('Users');
+    const user = {
+        userId: '<unique_user_id>', // Identifiant unique de l'utilisateur
+        username: '<username>', // Nom d'utilisateur choisi
+        email: '<email_address>', // Adresse e-mail de l'utilisateur
+        password: bcrypt.hashSync('<password>', 10), // Mot de passe hashé
+        createdAt: new Date(), // Horodatage de la création du compte
+    };
 
-const userSchema = new User.schema;
-const messageSchema = new Message.schema;
-const channelSchema = new Channel.schema;
-const fileUploadSchema = new FileUpload.schema;
-const privateMessageSchema = new PrivateMessage.schema;
-const userChannelSchema = new UserChannel.schema;
+    // Schéma de la collection Messages
+    const messages = db.collection('Messages');
+    const message = {
+        messageId: '<unique_message_id>', // Identifiant unique du message
+        content: '<message_content>', // Contenu textuel du message
+        senderId: '<sender_user_id>', // ID de l'utilisateur expéditeur
+        channelId: '<channel_id>', // ID du canal où le message est envoyé
+        createdAt: new Date() // Horodatage de l'envoi du message
+    };
 
-// Create Models
-const UserModel = mongoose.model('User', userSchema);
-const MessageModel = mongoose.model('Message', messageSchema);
-const ChannelModel = mongoose.model('Channel', channelSchema);
-const FileUploadModel = mongoose.model('FileUpload', fileUploadSchema);
-const PrivateMessageModel = mongoose.model('PrivateMessage', privateMessageSchema);
-const UserChannelModel = mongoose.model('UserChannel', userChannelSchema);
+    // Schéma de la collection Canaux
+    const channels = db.collection('Channels');
+    const channel = {
+        channelId: '<unique_channel_id>', // Identifiant unique du canal
+        name: '<channel_name>', // Nom du canal
+        members: ['<user_id_1>', '<user_id_2>'], // Liste des ID des utilisateurs membres
+        createdAt: new Date() // Horodatage de la création du canal
+    };
+
+    // Exemples d'insertions (décommentez pour utiliser)
+    // users.insertOne(user);
+    // messages.insertOne(message);
+    // channels.insertOne(channel);
+
+    client.close();
+});
