17b0f93531fd794cd818274a3a610bc39f0e7d95
Antoine <antoine.gourgue@epitech.eu>
2024-02-11 01:04:02 +0100

    feat: general channel and routing

--- app/utils/socketManager.js
+++ app/utils/socketManager.js
@@ -11,6 +11,11 @@ const init = (_io) => {
     io.on('connection', (socket) => {
         console.log(`Un utilisateur est connecté, ID Socket: ${socket.id}`);
 
+        socket.on('joinConnectedUsers', async (userId) => {
+            await addConnectUser(userId);
+        })
+
+        handleGeneralChatMessage(socket);
         emitConnectedUsers();
         attachLogoutListener(socket);
     });
@@ -31,17 +36,14 @@ const addConnectUser = async (userId) => {
     let connectedUser = connectedUsers.find(user => user.userId === userId);
 
     console.log({
-        userId,
-        connectedUser,
-        connectedUsers
+        userId, connectedUser, connectedUsers
     })
 
     if (!connectedUser) {
         let user = await User.findById(userId);
         // Nouvel utilisateur, l'ajouter à la liste
         connectedUser = {
-            userId: userId.toString(),
-            username: user?.username || 'Unknown', // Exemple de génération de nom
+            userId: userId.toString(), username: user?.username || 'Unknown', // Exemple de génération de nom
             image: 'https://avatars.githubusercontent.com/u/35387401?v=4', // URL d'image par défaut,
             isOnline: true
         };
@@ -60,7 +62,7 @@ const removeConnectedUser = (userId) => {
 }
 
 
-const attachLogoutListener  = (socket) => {
+const attachLogoutListener = (socket) => {
     socket.on('logout', (userId) => {
         // Optionnel : Marquez l'utilisateur comme déconnecté sans le supprimer immédiatement
         const connectedUser = connectedUsers.find(user => user.userId === userId);
@@ -74,9 +76,27 @@ const attachLogoutListener  = (socket) => {
     })
 }
 
+const handleGeneralChatMessage = (socket) => {
+    /**
+     *  Gestion des messages de chat général
+     *  payload: { user: { id: string, username: string, email: string }, text: string, timestamp: number }
+     */
+
+    socket.on('sendMessage', (payload) => {
+        const user = connectedUsers.find(user => user.userId === payload.user.id);
+
+        if (user) {
+            console.log(`Message reçu dans le canal général : ${payload.text} de ${user.username}`);
+
+            // Diffuser le message enrichi à tous les clients connectés, y compris l'expéditeur
+            io.emit('receiveMessage', payload);
+        } else {
+            console.log("Utilisateur non trouvé pour l'ID:", payload.userId);
+        }
+    });
+}
+
+
 module.exports = {
-    connectedUsers,
-    init,
-    addConnectUser,
-    attachLogoutListener
+    connectedUsers, init, addConnectUser, attachLogoutListener, handleGeneralChatMessage
 };
